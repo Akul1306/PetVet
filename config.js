c20f4d5fe1d33f7e9a6c9179a9c16a0b7856009d
Akul1306 <akul60314@gmail.com>
2026-02-24 22:11:45 +0530

    Remove config.js from repo

--- config.js
+++
@@ -1,9 +0,0 @@
-export const PORT = 3000;
-export const mongoDBURL =
-  "mongodb+srv://akul13:akul1314@natours.1hm8jyz.mongodb.net/?appName=Natours";
-// config.js
-export default {
-  JWT_SECRET: "my-max-secure-and-max-long-secret-school",
-  JWT_EXPIRES_IN: "90d",
-  // Add other configs as needed
-};
