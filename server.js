538bb3c4aabf28a5c9b908b711200a7f60583dee
Akul1306 <akul60314@gmail.com>
2026-02-24 22:06:30 +0530

    basic setup

---
+++ server.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { mongoDBURL, PORT } from "./config";
+import app from "./app.js";
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App is connected to database");
+    app.listen((PORT) => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
